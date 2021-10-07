b949bc5f4b490d56d779902eb3118e733ed9ef17
Chanpiseth Lim <limchanpiseth18@kit.edu.kh>
2021-10-07 18:24:28 +0700

    'cors'

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8080"
-};
+// var corsOptions = {
+//   origin: "http://localhost:8080"
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());  /* bodyParser.json() is deprecated */
